6b97ccb8509b7897706851fb9a19c5fa0e215c5f
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-07 13:16:14 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='sensor_response_curves',
-    version='0.4',
-    description='Sensor Response Curves for WV, S2, L8, L7',
+    version='0.5',
+    description='Sensor Response Curves for WV, GE, S2, L8, L7',
     author='Jonas Solvsteen',
-    author_email='josl@dhi-gras.com',
+    author_email='josl@dhigroup.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['pandas', 'scipy', 'numpy'])
